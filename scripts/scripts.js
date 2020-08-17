4b554166cf4067f18dde440ec97f943e4a0a6192
IftekharPriyo <priyo.mobin123@gmail.com>
2020-08-17 10:34:03 +0600

    list process

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,10 +11,23 @@ const loadArtistAndSong = (song) => {
     fetch(`https://api.lyrics.ovh/suggest/${song}`)
     .then(response => response.json())
     .then(info => {
+        const displayResult = document.getElementById('display-result');
+        displayResult.innerHTML ='';
         for(let i=0; i<10; i++){
-            console.log(info.data[i].title);
-            console.log(info.data[i].album.title);
-            console.log(info.data[i].artist.name);
+            let songName = info.data[i].title;
+            let songArtist = info.data[i].artist.name;
+            console.log(songName);
+            console.log(songArtist);
+            displayResult.innerHTML += 
+            `<div class="single-result row align-items-center my-3 p-3">
+            <div class="col-md-9">
+                <h3 class="lyrics-name">Purple Noon</h3>
+                <p class="author lead">Album by <span>Washed Out</span></p>
+            </div>
+            <div class="col-md-3 text-md-right text-center">
+                <button class="btn btn-success">Get Lyrics</button>
+            </div>
+        </div>`;
         }
     })
 }
