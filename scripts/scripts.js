32544ecf3525d507482dc453f74b6223e86158f5
IftekharPriyo <priyo.mobin123@gmail.com>
2020-08-17 12:56:46 +0600

    lyrics result works

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,18 @@
-
 const songSearch = document.getElementById('song-search');
 songSearch.addEventListener('click',()=>{
     const searchedSong = document.getElementById('song-name').value;
     loadArtistAndSong(searchedSong);
-})
+});
+
+const loadLyrics = (artist, title) =>{
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    .then(response => response.json())
+    .then(data =>{
+        document.getElementById('lyrics-artist-display').innerText = artist;
+        document.getElementById('lyrics-song-display').innerText = title;
+        document.getElementById('lyrics-display').innerText = data.lyrics;
+    })
+}
 
 
 
@@ -12,24 +21,24 @@ const loadArtistAndSong = (song) => {
     .then(response => response.json())
     .then(info => {
         const displayResult = document.getElementById('display-result');
-        displayResult.innerHTML ='';
+        displayResult.innerHTML = '';
         for(let i=0; i<10; i++){
             let songName = info.data[i].title;
             let songArtist = info.data[i].artist.name;
-            console.log(songName);
-            console.log(songArtist);
             displayResult.innerHTML += 
             `<div class="single-result row align-items-center my-3 p-3">
             <div class="col-md-9">
-                <h3 class="lyrics-name">Purple Noon</h3>
-                <p class="author lead">Album by <span>Washed Out</span></p>
+                <h3 class="lyrics-name">${songName}</h3>
+                <p class="author lead">Album by <span>${songArtist}</span></p>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+            <a href="#lyrics-artist-display" onclick="loadLyrics('${songArtist}','${songName}')" id="load-lyrics" class="btn btn-success">Get Lyrics</a>
             </div>
         </div>`;
+        
         }
     })
 }
+
 
 
