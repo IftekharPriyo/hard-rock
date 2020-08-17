318caa4eefdc71fd105997730bb890ad75ed2fba
IftekharPriyo <priyo.mobin123@gmail.com>
2020-08-17 14:00:50 +0600

    v0.8

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,24 @@
-const songSearch = document.getElementById('song-search');
-songSearch.addEventListener('click',()=>{
-    const searchedSong = document.getElementById('song-name').value;
-    loadArtistAndSong(searchedSong);
-});
+//initialization
+document.getElementById('display-result').style.display = 'none';
+document.getElementById('lyrics-section').style.display = 'none';
 
+
+// function to load lyrics of the songs from the search results
 const loadLyrics = (artist, title) =>{
-    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`) //API for lyrics
     .then(response => response.json())
     .then(data =>{
         document.getElementById('lyrics-artist-display').innerText = artist;
         document.getElementById('lyrics-song-display').innerText = title;
         document.getElementById('lyrics-display').innerText = data.lyrics;
     })
+    document.getElementById('lyrics-section').style.display = 'block';
 }
 
 
-
+//function to load the list of artist and song in the UI
 const loadArtistAndSong = (song) => {
-    fetch(`https://api.lyrics.ovh/suggest/${song}`)
+    fetch(`https://api.lyrics.ovh/suggest/${song}`) //API for retrieving song and artist name
     .then(response => response.json())
     .then(info => {
         const displayResult = document.getElementById('display-result');
@@ -25,20 +26,29 @@ const loadArtistAndSong = (song) => {
         for(let i=0; i<10; i++){
             let songName = info.data[i].title;
             let songArtist = info.data[i].artist.name;
+            let songAlbum = info.data[i].album.title;
             displayResult.innerHTML += 
             `<div class="single-result row align-items-center my-3 p-3">
             <div class="col-md-9">
                 <h3 class="lyrics-name">${songName}</h3>
-                <p class="author lead">Album by <span>${songArtist}</span></p>
+                <p class="author lead">Song by <span>${songArtist}</span></p>
+                <h6 class="author lead">Album : <span>${songAlbum}</span></h6>
             </div>
             <div class="col-md-3 text-md-right text-center">
             <a href="#lyrics-artist-display" onclick="loadLyrics('${songArtist}','${songName}')" id="load-lyrics" class="btn btn-success">Get Lyrics</a>
             </div>
         </div>`;
-        
+        document.getElementById('display-result').style.display = 'block';
         }
     })
 }
 
 
+// search button functionalities
+const songSearch = document.getElementById('song-search');
+songSearch.addEventListener('click',()=>{
+    const searchedSong = document.getElementById('song-name').value;
+    loadArtistAndSong(searchedSong);
+    document.getElementById('lyrics-section').style.display = 'none';
+});
 
